e5cff3591294d7e6fdb0dfef1e4ac068143fab76
Richard Rodriguez <richard.rodriguez@comparethemarket.com>
2016-04-05 22:09:52 +0100

    10 - implement real prime number validator

---
+++ FindMyPast.PrimeNumberTable.Tests/PrimeNumberValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace FindMyPast.PrimeNumberTable.Tests
+{
+    [TestFixture]
+    class PrimeNumberValidatorTests
+    {
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(7)]
+        [TestCase(11)]
+        [TestCase(13)]
+        [TestCase(17)]
+        [TestCase(19)]
+        [TestCase(23)]
+        [TestCase(379)]
+        [TestCase(379)]
+        public void TheseAreValidPrimeNumbers(int testValue)
+        {
+            var primeNumberValidator = new PrimeNumberValidator();
+
+            Assert.True(primeNumberValidator.IsPrime(testValue));
+        }
+    }
+}
---
+++ FindMyPast.PrimeNumberTable/PrimeNumberValidator.cs
@@ -0,0 +1,18 @@
+﻿namespace FindMyPast.PrimeNumberTable
+{
+    public class PrimeNumberValidator : IValidatePrimeNumbers
+    {
+        public bool IsPrime(int input)
+        {
+            for (var i = 2; i < input; i++)
+            {
+                var divisionRemainder = input%i;
+                if (divisionRemainder == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
